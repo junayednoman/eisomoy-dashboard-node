b60dddb06b16543c3d07bb7d735ac1ea3bbe5245
sabbir073 <md.sabbir073@gmail.com>
2024-03-03 17:02:47 +0600

    fixed

--- pages/add-news.tsx
+++ pages/add-news.tsx
@@ -172,11 +172,11 @@ const AddNews = () => {
                                         <AnimateHeight duration={50} height={active === 3 ? 'auto' : 0}>
                                             <div className="p-4 pt-2 font-semibold text-white-dark">
                                             {categories.map(category => {
-                                                console.log(category.categoryName);
+                                                console.log(category);
                                                 return (
-                                                    <label htmlFor={category.categoryName} className="flex items-center gap-[6px]">
-                                                        <Field name={category.categoryName} type="checkbox" id={category.cat_id} className="h-4 w-4" />
-                                                        <span>{category.categoryName}</span>
+                                                    <label htmlFor={category} className="flex items-center gap-[6px]">
+                                                        <Field name={category} type="checkbox" id={category} className="h-4 w-4" />
+                                                        <span>{category}</span>
                                                     </label>
                                                 );
                                             })}
