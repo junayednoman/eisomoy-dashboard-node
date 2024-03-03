22bcd91eac77933be9763b7c4b69eb7f6a153950
sabbir073 <md.sabbir073@gmail.com>
2024-03-03 08:52:20 +0600

    update delete added

--- pages/categories.tsx
+++ pages/categories.tsx
@@ -12,6 +12,7 @@ import Select from 'react-select';
 import AnimateHeight from 'react-animate-height';
 import axios from 'axios';
 import * as Yup from 'yup';
+import { Modal } from '@mantine/core';
 
 const validationSchema = Yup.object().shape({
     categoryName: Yup.string().required('Title is required'),
@@ -47,11 +48,18 @@ const Categories = () => {
     const [pageSize, setPageSize] = useState(PAGE_SIZES[0]);
     const [initialRecords, setInitialRecords] = useState<any[]>([]);
 
+    const [categoryDataToUpdate, setCategoryDataToUpdate] = useState<any>(null);
+
     const [search, setSearch] = useState('');
     const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({
         columnAccessor: 'categoryName',
         direction: 'asc',
     });
+
+    const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
+    const [error, setError] = useState('');
+    const [deleteCategory, setDeleteCategory] = useState(null);
+    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
 
     useEffect(() => {
         fetchData();
@@ -157,6 +165,89 @@ const Categories = () => {
         setActive((oldValue) => {
             return oldValue === value ? 0 : value;
         });
+    };
+
+    const handleUpdate = async (values: any) => {
+        try {
+            const response = await axios.post(
+                `${apiUrl}/api/user/update`,
+                values,
+                { withCredentials: true }
+            );
+            console.log(response.data); // Log the response from the API
+            setIsUpdateModalOpen(false); // Close the modal on successful update
+            Swal.fire({
+                icon: 'success',
+                title: 'Category Updated successfully',
+                timer: 3000,
+                showConfirmButton: false
+            });
+            fetchData(); // Refetch user data after updating
+        } catch (error: any) {
+            console.error('Error updating user:', error);
+            if (error.response) {
+                setError(error.response.data.message || 'Server Error');
+            } else {
+                setError('Something Went Wrong!');
+            }
+
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops... Something went wrong!',
+                text: error.response?.data?.message || 'Failed to Update category',
+                timer: 3000,
+                showConfirmButton: false
+            });
+        }
+    };
+
+    const handleDelete = async () => {
+        try {
+            const response = await axios.post(
+                `${apiUrl}/api/user/delete`,
+                { categoryName: deleteCategory },
+                { withCredentials: true }
+            );
+            console.log(response.data); // Log the response from the API
+            setShowDeleteConfirmation(false); // Close the confirmation modal
+            Swal.fire({
+                icon: 'success',
+                title: 'Category Deleted successfully',
+                timer: 3000,
+                showConfirmButton: false
+            });
+            fetchData(); // Refetch user data after deletion
+        } catch (error: any) {
+            console.error('Error deleting user:', error);
+            setShowDeleteConfirmation(false); // Close the confirmation modal
+            setError('Failed to delete user');
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops... Something went wrong!',
+                text: error.response?.data?.message || 'Failed to Delete category',
+                timer: 3000,
+                showConfirmButton: false
+            });
+        }
+    };
+
+    const handleOpenDeleteConfirmation = (categoryName: any) => {
+        setDeleteCategory(categoryName);
+        setShowDeleteConfirmation(true);
+    };
+
+    const handleCloseDeleteConfirmation = () => {
+        setShowDeleteConfirmation(false);
+    };
+
+    const handleOpenUpdateModal = (initialRecords: any) => {
+        setCategoryDataToUpdate(initialRecords);
+        setIsUpdateModalOpen(true);
+    };
+
+    const handleCloseUpdateModal = () => {
+        setCategoryDataToUpdate(null);
+        setIsUpdateModalOpen(false);
     };
 
     return (
@@ -296,14 +387,29 @@ const Categories = () => {
                                             title: 'News Count',
                                             sortable: true 
                                         },
+                                        { 
+                                            accessor: 'metaTitle', // Adjusted accessor to match the response data key
+                                            title: 'Meta Title',
+                                            sortable: true 
+                                        },
+                                        { 
+                                            accessor: 'metaDescription', // Adjusted accessor to match the response data key
+                                            title: 'Meta Desc',
+                                            sortable: true 
+                                        },
+                                        { 
+                                            accessor: 'focusKeyword', // Adjusted accessor to match the response data key
+                                            title: 'Keywords',
+                                            sortable: true 
+                                        },
                                         {
                                             accessor: 'action',
                                             title: 'Action',
                                             titleClassName: '!text-center',
-                                            render: () => (
+                                            render: (rowData) => (
                                                 <div className="mx-auto flex w-max items-center gap-2">
                                                     <Tippy content="Edit">
-                                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5">
+                                                        <svg onClick={() => handleOpenUpdateModal(rowData)} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5">
                                                             <path
                                                                 d="M15.2869 3.15178L14.3601 4.07866L5.83882 12.5999L5.83881 12.5999C5.26166 13.1771 4.97308 13.4656 4.7249 13.7838C4.43213 14.1592 4.18114 14.5653 3.97634 14.995C3.80273 15.3593 3.67368 15.7465 3.41556 16.5208L2.32181 19.8021L2.05445 20.6042C1.92743 20.9852 2.0266 21.4053 2.31063 21.6894C2.59466 21.9734 3.01478 22.0726 3.39584 21.9456L4.19792 21.6782L7.47918 20.5844L7.47919 20.5844C8.25353 20.3263 8.6407 20.1973 9.00498 20.0237C9.43469 19.8189 9.84082 19.5679 10.2162 19.2751C10.5344 19.0269 10.8229 18.7383 11.4001 18.1612L11.4001 18.1612L19.9213 9.63993L20.8482 8.71306C22.3839 7.17735 22.3839 4.68748 20.8482 3.15178C19.3125 1.61607 16.8226 1.61607 15.2869 3.15178Z"
                                                                 stroke="currentColor"
@@ -318,7 +424,7 @@ const Categories = () => {
                                                         </svg>
                                                     </Tippy>
                                                     <Tippy content="Delete">
-                                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5">
+                                                        <svg onClick={() => handleOpenDeleteConfirmation(rowData.categoryName)} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5">
                                                             <path
                                                                 opacity="0.5"
                                                                 d="M9.17065 4C9.58249 2.83481 10.6937 2 11.9999 2C13.3062 2 14.4174 2.83481 14.8292 4"
@@ -363,6 +469,65 @@ const Categories = () => {
                     </div>
                 </div>
             </div>
+            {categoryDataToUpdate && (
+    <Modal
+        className='dark:updateUserModal'
+        opened={isUpdateModalOpen}
+        onClose={handleCloseUpdateModal}
+        title="Update User"
+    >
+        <Formik initialValues={{ ...categoryDataToUpdate }} onSubmit={handleUpdate} validationSchema={validationSchema}>
+            {({ errors, touched }) => (
+                <Form>
+                    <input type="hidden" name="userid" id="userid" value={categoryDataToUpdate.userid} /> {/* Hidden input for userId */}
+
+                    <div className="mb-3">
+                        <label htmlFor="name">Name</label>
+                        <Field className="form-input h-10" type="text" id="name" name="name" placeholder="Enter User Name" />
+                        {errors.name && touched.name && <p className="text-red-500">{errors.name as string}</p>}
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email">Email</label>
+                        <Field className="form-input h-10" type="email" id="email" name="email" placeholder="Enter User Email" />
+                        {errors.email && touched.email && <p className="text-red-500">{errors.email as string}</p>}
+                    </div>
+                    <div className="mb-3">
+                            <label htmlFor="password">Password</label>
+                            <Field className="form-input h-10" type="password" id="password" name="password" placeholder="Enter Password" />
+                            {errors.password && touched.password && <p className="text-red-500">{errors.password as string}</p>}
+                        </div>
+                    <div className="mb-3">
+                        <label htmlFor="role">Role</label>
+                        <Field as="select" className="form-select h-10" id="role" name="role">
+                            <option value="admin">Admin</option>
+                            <option value="editor">Editor</option>
+                            <option value="reporter">Reporter</option>
+                        </Field>
+                        {errors.role && touched.role && <p className="text-red-500">{errors.role as string}</p>}
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="display_name">Display Name</label>
+                        <Field className="form-input h-10" type="text" id="display_name" name="display_name" placeholder="Enter Display Name" />
+                        {errors.display_name && touched.display_name && <p className="text-red-500">{errors.display_name as string}</p>}
+                    </div>
+                    {error && <p className="text-red-500">{error}</p>}
+                    <button type="submit" className="btn btn-primary !mt-6">
+                        Update User
+                    </button>
+                </Form>
+            )}
+        </Formik>
+    </Modal>
+    
+)}
+
+<Modal className='dark:addUserModal' opened={showDeleteConfirmation} onClose={handleCloseDeleteConfirmation} title="Confirm Deletion">
+                <p>Are you sure you want to delete this user?</p>
+                <div className="flex justify-end mt-4">
+                    <button onClick={handleCloseDeleteConfirmation} className="btn btn-secondary mr-2">Cancel</button>
+                    <button onClick={handleDelete} className="btn btn-danger">Delete</button>
+                </div>
+            </Modal>
         </div>
     );
 };
